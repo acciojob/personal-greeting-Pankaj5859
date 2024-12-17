534b7eedc6bf47254285a96e05fd5d8888ab2df5
Pankaj Jha <pankajjhawork@gmail.com>
2024-12-17 16:40:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[name,setName]=useState("")
+
   return (
     <div>
         {/* Do not remove the main div */}
+     <input type="text" className="form-control w-25 m-3 bg-light " placeholder="name" onChange={(e)=>{setName(e.target.value)}} />
+      {name ? <h2>My name is {name}</h2> : "" }
     </div>
   )
 }
